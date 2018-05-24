9b2dce18f5791a374feab2a48857acb048e3d9c5
timothylampung <tim950808136453>
2018-05-25 02:40:08 +0800

    sfdsdfsdfs

--- mobile/src/main/java/com/example/aliff/bookingsystemcomputerservice/UsedItemAdapter.java
+++ mobile/src/main/java/com/example/aliff/bookingsystemcomputerservice/UsedItemAdapter.java
@@ -2,80 +2,115 @@ package com.example.aliff.bookingsystemcomputerservice;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Aliff on 21/5/2018.
  */
 
-public class UsedItemAdapter extends ArrayAdapter<InvoiceModalInventory> {
-    String userid;
+public class UsedItemAdapter extends RecyclerView.Adapter<UsedItemAdapter.ViewHolder> {
+    private List<InvoiceModalInventory> items;
+    private int itemLayout;
+    private Context c;
+    private int minteger = 0;
 
-    public ArrayList<InvoiceModalInventory> dataSet;
-    Context mContext;
-    String accesslevel;
-
-    ArrayList<String> rootValues = new ArrayList<String>();
-    // View lookup cache
-    public static class ViewHolder {
-        TextView tvItem;
-        TextView tvQuantity;
-        TextView tvQuantityInNeed;
-
+    public UsedItemAdapter(Context context, List<InvoiceModalInventory> items, int itemLayout) {
+        this.items = items;
+        this.itemLayout = itemLayout;
+        this.c =context;
     }
-
-
-
-
-    public UsedItemAdapter(ArrayList<InvoiceModalInventory> data, Context context) {
-        super(context, R.layout.activity_diagnose_adapter, data);
-        this.dataSet = data;
-        this.mContext = context;
-    }
-
-    public int lastPosition = -1;
 
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        final InvoiceModalInventory dataModel = getItem(position);
-        UsedItemAdapter.ViewHolder viewHolder; // view lookup cache stored in tag
-
-        final View result;
-
-        if (convertView == null) {
-
-            viewHolder = new UsedItemAdapter.ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.activity_diagnose, parent, false);
-
-            viewHolder.tvItem = (TextView) convertView.findViewById(R.id.tvItemName);
-            viewHolder.tvQuantity = (TextView) convertView.findViewById(R.id.tvItemQuantity);
-
-
-            result = convertView;
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (UsedItemAdapter.ViewHolder) convertView.getTag();
-            result = convertView;
-
-        }
-        lastPosition = position;
-
-        viewHolder.tvItem.setText(dataModel.getItemName());
-        viewHolder.tvQuantity.setText(dataModel.getItemQuantity());
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(itemLayout, parent, false);
+        return new ViewHolder(v);
+    }
 
 
 
-        // Return the completed view to render on screen
-        return result;
+    @Override public void onBindViewHolder(final ViewHolder holder, final int position) {
+        final InvoiceModalInventory item = items.get(position);
 
+        holder.itemName.setText(item.getItemName());
+        holder.itemQuantity.setText(item.getItemQuantity());
+        holder.count.setText(String.valueOf(minteger));
+//        holder.supplier.setText(item.getDate());
+//        holder.count.setVisibility(View.GONE);
+        holder.itemView.setTag(item);
+
+
+
+       holder.addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            minteger=minteger+1;
+                holder.count.setText(String.valueOf(minteger));
+
+            }
+        });
+
+        holder.minusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                minteger=minteger-1;
+                holder.count.setText(String.valueOf(minteger));
+
+
+            }
+        });
 
     }
 
-}
+
+    public void add(InvoiceModalInventory item, int position) {
+        items.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(InvoiceModalInventory item) {
+        int position = items.indexOf(item);
+        items.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+      public TextView itemName,itemQuantity,count;
+      public Button addBtn,minusBtn;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            count=(TextView)itemView.findViewById(R.id.tvQuantityInNeed);
+           itemName =(TextView)itemView.findViewById(R.id.tvItemName);
+           itemQuantity=(TextView)itemView.findViewById(R.id.tvItemQuantity);
+           addBtn=(Button)itemView.findViewById(R.id.btnAddQuantity);
+           minusBtn=(Button)itemView.findViewById(R.id.btnSubtractQuantity);
+
+
+        }
+    }}
+
--- mobile/src/main/java/com/example/aliff/bookingsystemcomputerservice/diagnoseAdapter.java
+++ mobile/src/main/java/com/example/aliff/bookingsystemcomputerservice/diagnoseAdapter.java
@@ -1,24 +1,20 @@
 package com.example.aliff.bookingsystemcomputerservice;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.method.ScrollingMovementMethod;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.inputmethod.CompletionInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,26 +22,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class diagnoseAdapter extends AppCompatActivity{
 
-    private DatabaseReference myRef;
-
-
-    LinearLayout container;
-    private FirebaseAuth mAuth;
-    private Spinner areaSpinner;
-    private ArrayAdapter<String> areasAdapter;
-    private ArrayList<String> items = new ArrayList<>();
-    private ArrayList<InvoiceModalInventory> hold_item = new ArrayList<>();
-    private ArrayList<InvoiceModalInventory> selected_item = new ArrayList<>();
-
-    private TextView itemName,itemQuantity,QuantityInneed;
-    private LinearLayout linearLayout;
-
-    private ListView listView;
-
-    private ArrayAdapter<InvoiceModalInventory> arrayListAdapter;
+    private ArrayList<String> spinnerData = new ArrayList<>();
+    private AutoCompleteTextView   autoCompleteTextView;
+    private RecyclerView recyclerView;
+    private List<InvoiceModalInventory> hold_item = new ArrayList<>();
+    private List<InvoiceModalInventory> selected_item = new ArrayList<>();
+    private UsedItemAdapter usedItemAdapter;
+    private Button btn;
+    private int index = -1;
 
 
     @Override
@@ -54,40 +43,43 @@ public class diagnoseAdapter extends AppCompatActivity{
         setContentView(R.layout.activity_diagnose_adapter);
 
 
-
-        itemName=(TextView)findViewById(R.id.tvItemName);
-        itemQuantity=(TextView)findViewById(R.id.tvItemQuantity);
-        QuantityInneed=(TextView)findViewById(R.id.tvQuantityInNeed);
-        linearLayout = (LinearLayout)findViewById(R.id.holder2);
-
-        listView = findViewById(R.id.recyclerView);
-        selected_item.add(new InvoiceModalInventory("sdf","sdf","sdf","sdfsdf"));
-        arrayListAdapter = new UsedItemAdapter(selected_item, diagnoseAdapter.this);
-        listView.setAdapter(arrayListAdapter);
+        btn = findViewById(R.id.mBtnAdd);
+        recyclerView = (RecyclerView) findViewById(R.id.mRecyclerView);
+        usedItemAdapter = new UsedItemAdapter(getApplication(), selected_item, R.layout.activity_diagnose);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
 
-        areaSpinner = findViewById(R.id.spinnerSelectItemDiagnose);
-        areasAdapter = new ArrayAdapter<>(diagnoseAdapter.this, android.R.layout.simple_spinner_item, items);
-        areaSpinner.setAdapter(areasAdapter);
+        recyclerView.setAdapter(usedItemAdapter);
 
 
 
-        areaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+        autoCompleteTextView = (AutoCompleteTextView)findViewById(R.id.tvAutoComplete);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item, spinnerData);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        autoCompleteTextView.setAdapter(aa);
+        autoCompleteTextView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                areasAdapter.getItem(position);
-                arrayListAdapter.add(new InvoiceModalInventory(
-                        "",
-                        "",
-                        "",
-                        ""
-                ));
-                arrayListAdapter.notifyDataSetChanged();
-            };
+            public void onClick(View view) {
+                autoCompleteTextView.showDropDown();
+            }
+        });
 
+        autoCompleteTextView.setOnDismissListener(new AutoCompleteTextView.OnDismissListener() {
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onDismiss() {
 
+                Toast.makeText(getApplicationContext(),autoCompleteTextView.getListSelection()+"",Toast.LENGTH_SHORT).show();
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        usedItemAdapter.add(hold_item.get(autoCompleteTextView.getListSelection()+1),usedItemAdapter.getItemCount());
+                    }
+                });
             }
         });
 
@@ -103,22 +95,24 @@ public class diagnoseAdapter extends AppCompatActivity{
 
     }
 
+
+
+
+
     public void getSpinnerData() {
 
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-
-        myRef = database.getReference().child("inventory");
-
+        DatabaseReference myRef = database.getReference().child("inventory");
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot areaSnapshot : dataSnapshot.getChildren()) {
                     String areaName = areaSnapshot.getKey();
                     InvoiceModalInventory DATA = areaSnapshot.getValue(InvoiceModalInventory.class);
-                    hold_item.add(DATA);
-                    items.add(areaName);
+                   hold_item.add(DATA); // added list
+                    spinnerData.add(areaName);  // spinner's data
                 }
             }
 
@@ -133,21 +127,4 @@ public class diagnoseAdapter extends AppCompatActivity{
     }
 
 
-
-
-
-
-//    private void listAllAddView() {
-//        itemName.setText("");
-//
-//        int childCount = listView.getChildCount();
-//        for (int i = 0; i < childCount; i++) {
-//            View thisChild = listView.getChildAt(i);
-//            itemName.append(thisChild + "\n");
-//
-//            // AutoCompleteTextView childTextView = (AutoCompleteTextView) thisChild.findViewById(R.id.textout);
-//            //String childTextViewValue = childTextView.getText().toString();
-//            //itemName.append("= " + childTextViewValue + "\n");
-//        }
-//    }
 }
